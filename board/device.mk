0bcbc1c8c39e99e5d054b932cd3d540316b58be8
Mohd Faraz <mohd.faraz.abc@gmail.com>
2017-11-08 21:10:38 +0530

    G42: LineageOS-14.1

---
+++ board/device.mk
@@ -0,0 +1,9 @@
+# OTA assert
+TARGET_OTA_ASSERT_DEVICE := G42,InFocus,InFocus
+# Bionic
+
+# Enable Minikin text layout engine (will be the default soon)
+USE_MINIKIN := true
+
+# Configure jemalloc for low memory
+MALLOC_SVELTE := true
