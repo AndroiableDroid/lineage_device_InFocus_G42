e3d0bab16f990f331e36b1c9c7edf00c3029a2a8
Mohd Faraz <mohd.faraz.abc@gmail.com>
2017-12-10 17:06:18 +0530

    G42: Kernel Updated & Sepolicy

--- product/selinux.mk
+++ product/selinux.mk
@@ -1,4 +1,12 @@
-L_SEPOLICY := device/InFocus/G42/sepolicy
+# SELinux
+BOARD_SEPOLICY_DIRS := \
+       $(LOCAL_PATH)/sepolicy
+
+# Seccomp Filter
+BOARD_SECCOMP_POLICY := \
+$(LOCAL_PATH)/seccomp
+
+L_SEPOLICY := device/InFocus/G42/pres
 # SELinux
 PRODUCT_COPY_FILES += \
 	$(L_SEPOLICY)/file_contexts.bin:root/file_contexts.bin \
